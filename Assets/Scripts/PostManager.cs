c0e233b2faa5c76bb3b922a82a7887de59a69a87
mikepmerritt <mikepmerritt@gmail.com>
2023-02-27 22:16:43 -0500

    post manager now fetches posts from resources

--- Assets/Scripts/PostManager.cs
+++ Assets/Scripts/PostManager.cs
@@ -6,22 +6,42 @@ using TMPro;
 
 public class PostManager : MonoBehaviour
 {
-    public List<Post> dailyPosts;
+    public List<Post> dailyPosts, allPosts;
     public int curPost;
     public TMP_Text username, postText;
     public Image image;
+
     void Start()
     {
         username = GameObject.Find("Username").GetComponent<TMP_Text>();
         postText = GameObject.Find("Post Text").GetComponent<TMP_Text>();
         image = GameObject.Find("Post Image").GetComponent<Image>();
+
+        // TODO: need to change in the future when GenerateDailyPosts is updated
+        GenerateDailyPosts();
+        dailyPosts = allPosts;
+
         if(dailyPosts.Count > 0)
         {
             curPost = 0;
+            changePost(curPost);
         }
         else
         {
             Debug.LogError("No posts provided!");
+        }
+    }
+
+    public void GenerateDailyPosts()
+    {
+        // TODO: in the future, a lot more will happen in here
+        // This is where we will check post preconditions and make a random selection of which posts to use for a day
+        // For the time being, this will just return a testing set of posts, but more to come later!
+
+        Object[] fetchedPosts = Resources.LoadAll("Posts", typeof(Post));
+        foreach (Object o in fetchedPosts)
+        {
+            allPosts.Add((Post) o);
         }
     }
 
