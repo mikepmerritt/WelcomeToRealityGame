c8398afd709e706af92405511e3c2beeef86083e
mikepmerritt <mikepmerritt@gmail.com>
2023-03-06 17:43:41 -0500

    added screen for leaving comments

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -7,7 +7,7 @@ using TMPro;
 public class UIManager : MonoBehaviour
 {
     public GameObject posts, comments, leavingComments;
-    public GameObject content;
+    public GameObject commentContent;
     public LayoutElement layout;
     public PostManager pm;
     public GameObject commentPrefab;
@@ -16,7 +16,8 @@ public class UIManager : MonoBehaviour
     {
         posts = GameObject.Find("Post UI");
         comments = GameObject.Find("Comments UI");
-        content = comments.GetComponentInChildren<LayoutElement>().gameObject;
+        leavingComments = GameObject.Find("Leave Comment UI");
+        commentContent = comments.GetComponentInChildren<LayoutElement>().gameObject;
         layout = comments.GetComponentInChildren<LayoutElement>();
         pm = GameObject.Find("Post Manager").GetComponent<PostManager>();
         GoToPosts();
@@ -26,9 +27,10 @@ public class UIManager : MonoBehaviour
     {
         posts.SetActive(true);
         comments.SetActive(false);
+        leavingComments.SetActive(false);
         
         // clear all comments so they don't stick around for other posts
-        foreach(Transform childTransform in content.transform)
+        foreach(Transform childTransform in commentContent.transform)
         {
             GameObject.Destroy(childTransform.gameObject);
         }
@@ -38,12 +40,13 @@ public class UIManager : MonoBehaviour
     {
         comments.SetActive(true);
         posts.SetActive(false);
+        leavingComments.SetActive(false);
 
         // add comments to comments box
         layout.preferredHeight = pm.curPost.comments.Count * 100; // add space for comments in box
         foreach(Comment c in pm.curPost.comments)
         {
-            GameObject o = Instantiate(commentPrefab, content.transform);
+            GameObject o = Instantiate(commentPrefab, commentContent.transform);
             TMP_Text[] commentBoxes = o.GetComponentsInChildren<TMP_Text>();
             foreach(TMP_Text t in commentBoxes)
             {
@@ -56,6 +59,19 @@ public class UIManager : MonoBehaviour
                     t.text = c.commentText;
                 }
             }
+        }
+    }
+
+    public void GoToLeavingComments()
+    {
+        leavingComments.SetActive(true);
+        posts.SetActive(false);
+        comments.SetActive(false);
+        
+        // clear all comments so they don't stick around on screen
+        foreach(Transform childTransform in commentContent.transform)
+        {
+            GameObject.Destroy(childTransform.gameObject);
         }
     }
 }
